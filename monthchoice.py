d162174fd9189424a07d1b4f88ede00cb5429e04
muGrace <123809154+muGrace@users.noreply.github.com>
2024-04-10 22:51:40 +0300

    Add files via upload

---
+++ monthchoice.py
@@ -0,0 +1,35 @@
+#selecting message from a list of months
+month=input("enter month of your choice :\n")
+if month=="January":
+   msg="Hello snow!"
+elif month=="February":
+     msg="More snow"
+
+elif month=="March":
+     msg=" no More snow"
+elif month=="April":
+     msg="Almost Golf time!"
+
+elif month=="May":
+     msg=" time to golf"
+
+elif month=="June":
+     msg="school out"
+elif month=="July":
+     msg="happy fourth"
+elif month=="August":
+     msg="still golfing"
+elif month=="september":
+     msg="welcome back"
+elif month=="October":
+     msg="fall colors"
+elif month=="November":
+     msg="Turkey day"
+elif month=="December":
+     msg="Merry christmas"
+else:
+    msg=("invalid month")
+print(msg)
+
+
+
