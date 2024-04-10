88ecc5f357c8d591331d8c34d64329c9ea4bcd6f
muGrace <123809154+muGrace@users.noreply.github.com>
2024-04-10 23:00:21 +0300

    Add files via upload

---
+++ month.py
@@ -0,0 +1,5 @@
+#code to wish merry christmas when month entered is december and otherwise nhjappy new year
+month=str(input("Enter name of month:\n")) #prompt suser to enter month of their choice
+if month == "December":
+   print("Merry Christms") #if true returns merry christmas
+print("Happy new year") #returns happy new year for every month entered
